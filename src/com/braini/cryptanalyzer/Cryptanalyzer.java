df1448745790172be44233cb0d621b391e2f0e06
Илья Илюшечкин <iiv@braini.ru>
2022-11-16 20:03:55 +0300

    Corrected the output comments

--- src/com/braini/cryptanalyzer/Cryptanalyzer.java
+++ src/com/braini/cryptanalyzer/Cryptanalyzer.java
@@ -10,22 +10,17 @@ import java.util.regex.Pattern;
 
 public class Cryptanalyzer {
     public static void main(String[] args) {
-        String src = "C:\\Users\\IlyaI\\Desktop\\1\\3.txt";
-        String dest = "C:\\Users\\IlyaI\\Desktop\\1\\cripto.txt";
         String operation = args[0];                 // Получаем тип операции
-
-
-            if (operation.equals("encode") || operation.equals("decode")  ) {          // Проверяем тип операции
-                File file = new File(args[1]);
-                int key = Integer.parseInt(args[2]) % 73;       //Получаем ключ и делаем "нормализацию".
-                file.encoding(operation, key);
-            } else if ((operation.equals("bruteForce"))){
-                File file = new File(args[1], true);
-                file.bruteForce();
-            }
-
-
-
-
+        if (operation.equals("encode") || operation.equals("decode")) {          // Проверяем тип операции
+            File file = new File(args[1]);
+            int key = Integer.parseInt(args[2]) % 73;       //Получаем ключ и делаем "нормализацию".
+            file.encoding(operation, key);
+            System.out.println("Done");
+        } else if ((operation.equals("bruteForce"))) {
+            File file = new File(args[1], true);
+            file.bruteForce();
+            System.out.println("Done!");
+        }
+        System.out.println("Sorry, Argument error! Check and try again");
     }
 }
